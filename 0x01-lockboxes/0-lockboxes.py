5d54a919e39d6e23854f6ff86c3efcd9bd8775db
Abdul1091 <Abdul2kur1@gmail.com>
2024-10-10 02:04:05 -0700

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened. """
+    n = len(boxes)
+    myList = [0]
+    for i in myList:
+        for j in boxes[i]:
+            if j not in myList:
+                if j < n:
+                    myList.append(j)
+    if len(myList) == n:
+        return True
+    return False
